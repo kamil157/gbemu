fbd8bc958657df75a7152a03f59e009501825b77
Kamil Chlodnicki <kamilchlodnicki@gmail.com>
2019-03-29 17:42:58 +0100

    Add load operations

--- main.cpp
+++ main.cpp
@@ -13,45 +13,45 @@ std::string disassemble(const std::vector<uint8_t>& code, uint& pc)
     ss << fmt::format("{:04x} ", pc);
     switch (code[pc]) {
     case 0x00:
+        // No operation.
         ss << "NOP";
         break;
     case 0x01:
-        ss << fmt::format("LXI    B,#${:02x}{:02x}", code[pc + 2], code[pc + 1]);
-        opbytes = 3;
-        break;
-    case 0x02:
-        ss << "STAX   B";
-        break;
-    case 0x03:
-        ss << "INX    B";
-        break;
-    case 0x04:
-        ss << "INR    B";
-        break;
-    case 0x05:
-        ss << "DCR    B";
+        // Put value nn into BC.
+        ss << fmt::format("LD     BC,{:02x}{:02x}", code[pc + 2], code[pc + 1]);
         break;
     case 0x06:
-        ss << fmt::format("MVI    B,#${:02x}", code[pc + 1]);
+        // Put value B into memory at n.
+        ss << fmt::format("LD     B,{:02x}", code[pc + 1]);
         opbytes = 2;
         break;
-    case 0x07:
-        ss << "RLC";
+    case 0x2A:
+        // Put value at address HL into A. Increment HL.
+        // Same as: LD A,(HL) - INC HL
+        ss << "LDI    A,(HL)";
         break;
-    case 0x08:
-        ss << "NOP";
+    case 0x3A:
+        // Put value at address HL into A. Decrement HL.
+        // Same as: LD A,(HL) - DEC HL
+        ss << "LDD    A,(HL)";
         break;
-    /* ........ */
-    case 0x3e:
-        ss << fmt::format("MVI    A,#${:02x}", code[pc + 1]);
-        opbytes = 2;
+    case 0x47:
+        // Put value A into B.
+        ss << "LD     B,A";
         break;
-    /* ........ */
-    case 0xc3:
-        ss << fmt::format("JMP    ${:02x}{:02x}", code[pc + 2], code[pc + 1]);
-        opbytes = 3;
+    case 0x78:
+        // Put value B into A.
+        ss << "LD     A,B";
         break;
-        /* ........ */
+    case 0xE0:
+        // Put A into memory address $FF00+n.
+        ss << fmt::format("LD     ($FF00+{:02x}),A", code[pc + 1]);
+        break;
+    case 0xF2:
+        // Put value at address $FF00 + register C into A.
+        // Same as: LD A,($FF00+C)
+        ss << "LD     A,(C)";
+        break;
     }
 
     pc += opbytes;
