1977c00e8ad7dd3c364d1ba282fd675fd2f89675
Kamil Chlodnicki <kamilchlodnicki@gmail.com>
2019-04-21 14:18:07 +0200

    Dont use qt in preparing image

--- src/gui.cpp
+++ src/gui.cpp
@@ -18,6 +18,10 @@ Gui::Gui(const Emulator& emu, QWidget* parent)
     timer->start(1000 / 5);
 }
 
+struct Color {
+    uint8_t r, g, b;
+};
+
 void Gui::redraw()
 {
     static const auto linesPerTile = 8;
@@ -25,7 +29,9 @@ void Gui::redraw()
     static const auto tilesPerRow = 16;
 
     auto vram = emulator.getMmu().getVram();
-    QImage image(128, 256, QImage::Format_RGB32);
+    auto w = 128u;
+    auto h = 256u;
+    std::vector<uint8_t> buffer(4 * w * h, 0);
     for (auto tileY = 0; tileY < 32; ++tileY) {
         for (auto tileX = 0; tileX < tilesPerRow; ++tileX) {
             for (auto line = 0; line < linesPerTile; ++line) {
@@ -36,17 +42,20 @@ void Gui::redraw()
                 for (auto pixel = pixelsPerLine - 1; pixel >= 0; --pixel) {
                     auto bit1 = (byte1 >> pixel) & 1;
                     auto bit0 = (byte0 >> pixel) & 1;
-                    auto color = (bit0 << 1) + bit1;
+                    auto colorIndex = (bit0 << 1) + bit1;
 
-                    static const std::vector<QColor> colorMap = { Qt::white, Qt::darkGray, Qt::gray, Qt::black };
-                    QColor qColor = colorMap.at(static_cast<uint8_t>(color));
+                    static const std::vector<Color> colorMap = { { 255, 255, 255 }, { 170, 170, 170 }, { 85, 85, 85 }, { 0, 0, 0 } };
+                    Color color = colorMap.at(static_cast<uint8_t>(colorIndex));
                     auto x = tileX * pixelsPerLine + pixelsPerLine - 1 - pixel;
                     auto y = tileY * linesPerTile + line;
-                    image.setPixelColor(x, y, qColor);
+                    buffer[4 * (y * w + x)] = color.r;
+                    buffer[4 * (y * w + x) + 1] = color.g;
+                    buffer[4 * (y * w + x) + 2] = color.b;
                 }
             }
         }
     }
+    QImage image(buffer.data(), w, h, QImage::Format_RGB32);
     QPixmap pixmap = QPixmap::fromImage(image.scaled(256, 512));
     ui->labelVram->setPixmap(pixmap);
 }
